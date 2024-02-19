84c992d1adf249423df883060fbd2e1d5ba3fa36
patrickpflughaupt <patrickpflughaupt@gmail.com>
2024-02-19 09:15:53 +0000

    directory placehodlers

--- run_all_setup_files.sh
+++ run_all_setup_files.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# directory placehodlers
+mkdir -p ./COSMIC/data/{annotations,BaseTable,COSMIC,experiments,kmertone}
+mkdir -p ./data/{experiments,human_viruses,kmertone,liftover,models,parameters,range_effects,ranges,ref,setup,TFBS}
+
 cd ./setup/
 
 # install relevant software
